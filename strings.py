f691cfad1f732712f37a4e394139c9faf91d08ba
UpasanaRani <upasana00078@gmail.com>
2025-12-01 18:02:19 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,17 @@
+name="upasana"
+friend="roshan"
+anotherfriend="khusboo"
+apple='he said,i want to eat an apple'
+print("hello,"+name)
+print(apple)
+#print(apple)
+print(name[0])
+print(name[1])
+print(name[2])
+print(name[3])
+print(name[4])
+print(name[5])
+#print(name[7] will not be printed and will show error.)
+print("we are using a for loop to make it more easier.")
+for character in name:
+    print(character)
---
+++ stringslicing.py
@@ -0,0 +1,22 @@
+#length of a string
+names= "upasana,shristi"
+print(len(names))
+
+# another example
+fruit="Mango"
+mangoLen=len(fruit)
+print(mangoLen)#print 5
+print(fruit[0:4])#print mang
+print(fruit[1:4])#print ang
+print(fruit[:5])#automatically takes starting as zero and prints mango
+print(fruit[0:-3])
+print(fruit[0:-5])
+print(fruit[-3:-1])#[5-3:5-1]=[2:4]
+
+#quick quiz
+#nm="Harry"
+#print(nm[-4:-2])
+#length of harry=5
+#[1:3]#prints ar
+nm="harry"
+print(nm[-4:-2])
